1243c8ea8699c4e9cdb00ad276f67d3bcf5f2e4c
Ahmad Sharjeel <ahmadsharjeel86300@gmail.com>
2025-03-11 03:38:54 +0530

    Initial commit: ABX Exchange Client

---
+++ CMakeFiles/abx_client.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/abx_client.dir/src/Client.cpp.o"
+  "CMakeFiles/abx_client.dir/src/Client.cpp.o.d"
+  "CMakeFiles/abx_client.dir/src/main.cpp.o"
+  "CMakeFiles/abx_client.dir/src/main.cpp.o.d"
+  "abx_client"
+  "abx_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/abx_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/abx_client.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for abx_client.
+# This may be replaced when dependencies are built.
